6a57d37a4f73b11e0f93cf5cfbe0f3c941cca93b
BrankoMaljkovic <brankomaljkovic@yahoo.com>
2024-05-27 16:08:48 +0200

    Branko Team

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function Team() {
-    console.log(`team`)
+
+    const [teamDetail, setTeamDetails] = useState([]); // http://ergast.com/api/f1/' + year + '/constructors/' + id + '/constructorStandings.json' 
+    const [teamResults, setTeamResults] = useState([]);
+
+    useEffect(() => {
+        // console.log("useEffect"); poziva se funkcija
+        getTeamDetails();
+    },[]);
+
+    const getTeamDetails = async () => {
+        // console.log("getTeamDetails"); // radi funkcija
+        const url = "http://ergast.com/api/f1/2013/constructors/Ferrari/constructorStandings.json"
+        // console.log(url); // dobavlja URL
+        const response = await axios.get(url);
+        console.log(`response`, response.data);
+
+    };
+
     return(
-        <h1>Strana team</h1>
+        <h1>Strana team Branko</h1>
     )
 
 
