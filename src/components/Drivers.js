3c074ce5b2cf94f29c342d33d30c23f3404e2ec4
KristinaSabo <sabo.kristina@gmail.com>
2024-05-27 16:05:48 +0200

    drivers component

--- src/components/Drivers.js
+++ src/components/Drivers.js
@@ -1,8 +1,55 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function Drivers() {
-    console.log(`drivers`)
-    return(
-        <h1>Strana Drivers</h1>
+    console.log('drivers');
+
+
+    const [drivers, setDrivers] = useState([]);
+
+
+    useEffect(() => {
+        getDrivers();
+    }, []);
+
+
+    const getDrivers = async () => {
+        const url = "http://ergast.com/api/f1/2013/driverStandings.json";
+        // console.log(url);
+        const response = await axios.get(url);
+        console.log(`test`, response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+        setDrivers(response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+    }
+
+    return (
+        <div>
+            <h1>Drivers Championship</h1>
+            <table>
+                <thead>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>4</th>
+                </thead>
+                <tbody>
+                    {drivers.map((driver, i) => {
+                        console.log(driver);
+                        return (
+                            <tr>
+                            <td>{driver.position}</td>
+                            <td>{driver.Driver.givenName} {driver.Driver.familyName}</td>
+                            <td>{driver.Constructors.constructorId}</td>
+                            <td>{driver.points}</td>
+                        </tr>
+                        )
+ 
+                    }
+                    )}
+                </tbody>
+            </table>
+        </div>
     )
-
-
 }
+
+
+
