876b5823a226807a5db810b6845efec074445fd6
BrankoMaljkovic <brankomaljkovic@yahoo.com>
2024-05-27 14:12:31 +0200

    links added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Driver from "./components/Driver";
 import Drivers from "./components/Drivers";
 import Teams from "./components/Teams";
 import Team from "./components/Team";
+import Races from "./components/Races";
+import Race from "./components/Race";
 
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Marko</h1>
-      <Driver />
+      <Router>
+                <nav className="top-navigation" >
+                    <ul>
+                        <li>
+                            <Link to="/">Comments</Link>
+                            <Link to="/driver">Comments</Link>
+                            <Link to="/teams">Comments</Link>
+                            <Link to="/team">Comments</Link>
+                            <Link to="/races">Comments</Link>
+                            <Link to="/race">Comments</Link>
+                        </li>
+                    </ul>
+                </nav>
+            <Routes>
+                <Route path="/" element={<Drivers />} />
+                <Route path="/driver" element={<Driver />} />
+                <Route path="/teams" element={<Teams />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="/races" element={<Races />} />
+                <Route path="/race" element={<Race />} />
+            </Routes>
 
-      <h1>Sabo</h1>
-      <Drivers />
-
-      <h1>Kaca</h1>
-      <Teams />
-
-      <h1>Branko</h1>
-      <Team />
+            </Router>
 
     </div>
   );
 }
-
-export default App;
