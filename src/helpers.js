17234e46b99824f18d0f14f86e9d5d1d75071e9b
KristinaSabo <sabo.kristina@gmail.com>
2024-05-29 15:06:56 +0200

    race flags & helper update

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,6 @@
 export function getFlagCode(flags, value){
     const country = flags.find(
-        (country) => country.nationality === value
+        (country) => country.nationality === value ||country.en_short_name === value
     );
     return country ? country.alpha_2_code : '';
 }
