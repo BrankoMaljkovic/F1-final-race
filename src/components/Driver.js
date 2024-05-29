319f6e97a39e08fb57097afc16574ebf11fdd167
MarozaMarko <markopavlovic0111@gmail.com>
2024-05-29 11:10:09 +0200

    dodato dinamicko prikazivanje drivera

--- src/components/Driver.js
+++ src/components/Driver.js
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
 export default function Driver() {
     const [driverDetails, setDriverDetails] = useState({});
     const [driverRaces, setDriverRaces] = useState([]);
     const [loading, setLoading] = useState(true);
+    const { driverId } = useParams();
 
 
 
 
     useEffect(() => {
         const getDriverDetails = async () => {
-            const url = ("http://ergast.com/api/f1/2013/drivers/alonso/driverStandings.json");
+            const url = (`http://ergast.com/api/f1/2013/drivers/${driverId}/driverStandings.json`);
             const response = await axios.get(url);
             console.log("response final", response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]);
 
