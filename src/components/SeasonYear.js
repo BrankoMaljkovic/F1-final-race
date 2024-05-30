67f88d770a3c2aca848ed772a4c2401f96fbaeaf
BrankoMaljkovic <brankomaljkovic@yahoo.com>
2024-05-30 15:56:48 +0200

    SeasonYear

--- src/components/SeasonYear.js
+++ src/components/SeasonYear.js
@@ -1,9 +1,27 @@
+import React from 'react';
+import { DatePicker, Space } from 'antd';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+
+dayjs.extend(customParseFormat);
+const dateFormat = 'YYYY';
+
 export default function SeasonYear() { // treba staviti map za izvlacenje godina
+    const onChange = (date, dateString) => {
+        console.log(date, dateString);
+    };
+
+    const todayYear = new Date().getFullYear();
+
     return (
         <span>
-            <select>
-                <option></option>
-            </select>
+
+            <Space direction="vertical">
+                <DatePicker onChange={onChange} picker="year" 
+                minDate={dayjs('2000', dateFormat)}
+                maxDate={dayjs(`${todayYear}`, dateFormat)}/>
+            </Space>
+
         </span>
     )
 }
