5f741b16d315937012cd19b5805f230f8c0b2ccc
Kasijaa <handmadebykasija@icloud.com>
2024-05-31 10:40:45 +0200

    redizajn navigacije

--- src/App.js
+++ src/App.js
@@ -17,17 +17,18 @@ export default function App() {
   }, []);
 
   const getFlagsList = async () => {
-    const url = "https://raw.githubusercontent.com/Dinuks/country-nationality-list/master/countries.json";
+    const url =
+      'https://raw.githubusercontent.com/Dinuks/country-nationality-list/master/countries.json';
 
     const response = await axios.get(url);
     setFlagsList(response.data);
-  }
+  };
 
   return (
     <div className='App'>
       <Router>
         <nav className='top-navigation'>
-          <img src='../img/Logo.jpg' alt="" />
+          <img src='../img/Logo.jpg' alt='' />
           <ul>
             <li>
               <Link to='/'>Drivers</Link>
@@ -35,27 +36,26 @@ export default function App() {
               <Link to='/races'>Races</Link>
             </li>
           </ul>
-        </nav>
-        
-        <div className='serach-main-view'>
-        <div className="search">
-          <input type="text" placeholder='Search..' />
           <SeasonYear />
-        </div>
+        </nav>
 
-        <div className="main-view">
+        <div className='main-view'>
           <Routes>
             <Route path='/' element={<Drivers flags={flagsList} />} />
-            <Route path='/driverDetails/:driverId' element={<Driver flags={flagsList} />} />
+            <Route
+              path='/driverDetails/:driverId'
+              element={<Driver flags={flagsList} />}
+            />
             <Route path='/teams' element={<Teams flags={flagsList} />} />
-            <Route path='/teamDetails/:teamId' element={<Team flags={flagsList} />} />
+            <Route
+              path='/teamDetails/:teamId'
+              element={<Team flags={flagsList} />}
+            />
             <Route path='/races' element={<Races flags={flagsList} />} />
             <Route path='/race/:raceId' element={<Race flags={flagsList} />} />
           </Routes>
         </div>
-        </div>
       </Router>
-
     </div>
   );
 }
