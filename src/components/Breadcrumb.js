f1f9eec455e7adbd77fc4163c223d6d5bba2142c
BrankoMaljkovic <brankomaljkovic@yahoo.com>
2024-06-04 15:59:34 +0200

    breadcrumb

---
+++ src/components/Breadcrumb.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Breadcrumbs = ({ breadcrumbs }) => {
+return (
+<div className='breadcrumbs'>
+{breadcrumbs.map((breadcrumb, index) => (
+<span key={index}>
+{index > 0 && <span className='separator'>/</span>}
+{breadcrumb.link ? (
+<Link to={breadcrumb.link}>{breadcrumb.label}</Link>
+) : (
+<span>{breadcrumb.label}</span>
+)}
+</span>
+))}
+</div>
+);
+};
+
+export default Breadcrumbs;
+
+// staviti na svaku komponentu import Breadcrumb from './components/Breadcrumb';
+
+// odredjujemo sta pisemo
+// const breadcrumbs = [
+//     {label: 'Home'},
+//     { label: 'Drivers', link: '/' },
+//     { label: 'Teams', link: '/teams '},
+//     { label: 'Races', link: '/races' },
+//     ];
+
+
+// pozvati u svakom komponenti
+// <Breadcrumb />
